4e1b8c40f19fd138182d1d9c0f1b9d9662d624ca
kmg731 <kyle.goldbeck@gmail.com>
2021-08-19 02:15:20 -0400

    feat(ops): finish all logical ops, including half and full adder

--- logic.scm
+++ logic.scm
@@ -1,7 +1,67 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; \ab.a
 (define TRUE
   (lambda (a b)
-    a))
+    a)
+  )
 
+;; \fab.fba
+(define n
+  (lambda (f a b)
+    (f b a)
+    )
+  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; \ab.b or (\fab.ab)(\ab.a)
 (define FALSE
-  (lambda (a b)
-    b))
+  (n TRUE TRUE FALSE))
+
+;;; \pq.pqp
+(define a
+  (lambda (p q)
+    (p q p)
+    )
+  )
+
+;;; \pq.ppq
+(define OR
+  (lambda (p q)
+    (p p q)
+    )
+  )
+
+;;; \pq.NOT(p)NOT(p)q
+(define IFTHEN
+  (lambda (p q)
+    (n p (n p TRUE FALSE) q)
+    )
+  )
+
+;;; \pq.p(NOT(AND pq) q)
+(define XOR
+  (lambda (p q)
+    (p (n (a p q) TRUE FALSE) q)
+    )
+  )
+
+;;; \pq.p(qpq)(pqp)
+(define EQ
+  (lambda (p q)
+    (p (q p q) (p q p))
+    )
+  )
+
+;; Half Adder
+(define half-adder
+  (lambda (p q)
+    (list (a p q) (xor p q))))
+
+;; Full adder
+(define full-adder
+  (lambda (p q)
+    (list
+     (xor (xor x y) z) (o (a x y) (a (xor a b) c))
+    )
+  )
